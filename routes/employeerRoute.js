0b82d801034147c554a33808200398c32bf85157
yaklesh0007 <yaklesh0007@gmail.com>
2021-07-07 23:15:40 +0545

    added login function

--- routes/employeerRoute.js
+++ routes/employeerRoute.js
@@ -7,4 +7,27 @@ const jwt=require('jsonwebtoken')
 const authentication=require('../middlewares/authentication')
 const upload=require('../middlewares/uploads')
 
+router.post('/user/login',function(req,res){
+    Employeer.findOne({Email:req.body.Email})
+    .then(function(userData){
+        if(userData===null){
+           return res.status(401).json({message:"Authentication fail"})
+        }
+        else if(userData.Approved===false)
+        {
+            return res.status(403).json({message:"unApproved user"})
+        }
+        bcryptjs.compare(req.body.Password,userData.Password,function(err,cresult){
+            if(cresult===false){
+              return  res.status(401).json({message:" unAuthorized user"})
+            }
+           const token= jwt.sign({uid:userData._id},'secretkey');
+           res.status(200).json({success:true,token:token,message:"login Successful", userData})
+        })
+    })
+    .catch(function(err){
+        res.status(403).json({message:err})
+    })
+})
+
 module.exports=router
