763053a21e4e3cad5667f06966d592e05b3f455e
yaklesh0007 <yaklesh0007@gmail.com>
2021-07-07 22:54:03 +0545

    added register code

--- routes/employeerRoute.js
+++ routes/employeerRoute.js
@@ -7,4 +7,63 @@ const jwt=require('jsonwebtoken')
 const authentication=require('../middlewares/authentication')
 const upload=require('../middlewares/uploads')
 
+
+router.post('/employer/register',[
+    check('Email',"Email is required!").not().isEmpty(),
+    check('Email',"It is not valid email").isEmail(),
+    check('Fullname',"Fullname shouldnot be empty").not().isEmpty(),
+    check('Password',"password should not be empty!!!").not().isEmpty(),
+    check('Phone',"Phone should not be empty !!").not().isEmpty(),
+    
+],upload.single('Image'),upload.single('Citizenship'),function(req,res){
+    
+    const errors=validationResult(req);
+    
+
+    if(errors.isEmpty()){
+        
+        const Email=req.body.Email;
+        
+        const Fullname=req.body.Fullname;
+        const Age=req.body.Age;
+        const Location=req.body.Location;
+        const Password=req.body.Password;
+        const Contact=req.body.Contact;
+        const Image=req.file.filename;
+        const Citizenship=req.file.filename;
+        const Gender=req.body.gender;
+        
+        bcryptjs.hash(Password,10,function(err,hash){
+            const data=new Employeer({
+                Fullname:Fullname,
+                Contact:Contact,
+                Email:Email,
+                Password:hash,
+                Image:Image,
+                Gender:Gender,
+                Age:Age,
+                Location:Location,
+                Citizenship:Citizenship
+                
+            })
+            data.save()
+            .then(function(result){
+                res.status(201).json({success:true,result})
+
+            })
+            .catch(function(e){
+                // console.log(e)
+                res.status(500).json({message:e,success:false})
+                
+            })
+        
+        })
+    
+    }
+    else{
+        res.status(400).json(errors.array())
+    }
+   
+})
+
 module.exports=router
