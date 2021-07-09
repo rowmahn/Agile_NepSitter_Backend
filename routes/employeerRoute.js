f713fb33177709cd771d18e317541470d34274a4
yaklesh0007 <yaklesh0007@gmail.com>
2021-07-09 23:35:14 +0545

    makes changes in validation and remove file upload

--- routes/employeerRoute.js
+++ routes/employeerRoute.js
@@ -8,19 +8,25 @@ const authentication=require('../middlewares/authentication')
 const upload=require('../middlewares/uploads')
 
 
-router.post('/employer/register',[
-    check('Email',"Email is required!").not().isEmpty(),
-    check('Email',"It is not valid email").isEmail(),
-    check('Fullname',"Fullname shouldnot be empty").not().isEmpty(),
-    check('Password',"password should not be empty!!!").not().isEmpty(),
-    check('Contact',"Phone should not be empty !!").not().isEmpty(),
+router.post('/employer/register',
+// [
+//     check('Email',"Email is required!").not().isEmpty(),
+//     check('Email',"It is not valid email").isEmail(),
+//     check('Fullname',"Fullname shouldnot be empty").not().isEmpty(),
+//     check('Password',"password should not be empty!!!").not().isEmpty(),
+//     check('Contact',"Phone should not be empty !!").not().isEmpty(),
     
-],upload.single('Citizenship'),function(req,res){
+// ],
+// upload.single('Citizenship'),
+function(req,res){
     
-    const errors=validationResult(req);
+    // const errors=validationResult(req);
     
-
-    if(errors.isEmpty()){
+console.log(req.body)
+    // if(errors.isEmpty()){
+        // if(req.file==undefined){
+        //     return res.status(400).json({message:"invalid image Type!!"})
+        // }
         
         const Email=req.body.Email;
         
@@ -30,7 +36,7 @@ router.post('/employer/register',[
         const Password=req.body.Password;
         const Contact=req.body.Contact;
         // const Image=req.file.filename;
-        const Citizenship=req.file.filename;
+        const Citizenship=req.body.Citizenship;
         const Gender=req.body.Gender;
         
         bcryptjs.hash(Password,10,function(err,hash){
@@ -47,22 +53,22 @@ router.post('/employer/register',[
                 
             })
             data.save()
-            .then(function(result){
-                res.status(201).json({success:true,result})
+            .then(function(data){
+                res.status(201).json({success:true,data})
 
             })
             .catch(function(e){
-                // console.log(e)
+                
                 res.status(500).json({message:e,success:false})
                 
             })
         
         })
     
-    }
-    else{
-        res.status(400).json(errors.array())
-    }
+    // }
+    // else{
+    //     res.status(400).json(errors.array())
+    // }
    
 })
 
