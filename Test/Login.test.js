76bd147bdb7d78f73c2e98651b72ed4cab07b807
yaklesh0007 <yaklesh0007@gmail.com>
2021-07-10 18:02:42 +0545

    apply for job tdd testing

--- Test/Login.test.js
+++ Test/Login.test.js
@@ -27,11 +27,42 @@ describe('Test for Worker Login', () =>{
 //     expect(pro_ret.Tendername).toEqual('Building construction');
 // });
 // });
+it('Register Worker  testing anything', () => {
+    const Worker = {
+    'fname': 'Nokia',
+    'lname':'nuron',
+    'email': 'jhonsmith12@gmail.com',
+    'password':'Social',
+    'phone':'8031212311',
+    'gender':'Male',
+    'address':'Yogikuti',
+    'province':'state 4',
+    'certificate':'+2',
+    'yourself':'hjbbadbjbjadsb',
+    'study':'true',
+    'status':'married',
+    'smoke':'true',
+    'drink':'true',
+    'jobcategory':'baby sitting',
+    'availabilityMorning':['sunday','monday'],
+    'availabilityEvening':['sunday','monday'],
+    'availabilityAfternoon':['sunday','monday'],
+    'availabilityNight':['sunday','monday'],
+    'bdate':'19/01/2000',
+    'district':'Rupandehi',
+    'city':'butwal'
 
-it('Test to login', async() =>{
-    const status= await Worker.find({_id:Object('607ed31c58d3ab18501278ce')});
-    expect(status.ok)
-    });
+    };
+    return Workerlogin.create(Worker)
+    .then((pro_ret) => {
+    expect(pro_ret.fname).toEqual('Nokia');
+    })
+
+})
+// it('Test to login', async() =>{
+//     const status= await Worker.find({_id:Object('607ed31c58d3ab18501278ce')});
+//     expect(status.ok)
+//     });
  
 
 })
