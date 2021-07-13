f09cbc756c70cb4959d74e88af293343dad65201
yaklesh0007 <yaklesh0007@gmail.com>
2021-07-13 16:21:27 +0545

    added authentication for Worker

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,6 +1,6 @@
 const jwt=require('jsonwebtoken')
 const Employer=require('../models/Employeer')
-
+const Worker=require('../models/Applyforjob')
 //check for token
 
 module.exports.verifyEmployer=function(req,res,next){
@@ -14,7 +14,33 @@ module.exports.verifyEmployer=function(req,res,next){
         Employer.findOne({_id:data.uid})
         .then(function(result){
            
-             req.eployer=result
+             req.employer=result
+            next()
+    })
+    .catch(function(ex){
+        res.status(403).json(ex)
+    })
+    }
+    catch(e){
+    
+        res.status(403).json({error:e})
+        
+    }
+    
+}
+
+module.exports.verifyWorker=function(req,res,next){
+    
+     
+    try{
+        
+        const token=req.headers.authorization.split(" ")[1];
+        
+        const data=jwt.verify(token, 'secretkey');
+        Worker.findOne({_id:data.wid})
+        .then(function(result){
+           
+             req.worker=result
             next()
     })
     .catch(function(ex){
