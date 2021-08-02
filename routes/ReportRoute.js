b63995ff78fbcb1fe0834fea2fbc701855c8d7fd
rowmahn <dulalroman1@gmail.com>
2021-08-02 18:16:22 +0545

    report employer commit

--- routes/ReportRoute.js
+++ routes/ReportRoute.js
@@ -36,4 +36,37 @@ router.get('/worker/reports',function(req,res,next){
     
 
 })
+router.post('/employer/report/:empid', function(req,res){
+    const empid=req.params.empid;
+    const title= req.body.title;
+    const issue= req.body.issue;
+    const data=new Report({
+        empid:empid,
+        title:title, 
+        issue:issue
+    })
+    data.save()
+    .then(function(data){
+        res.status(201).json({success:true,data})
+
+    })
+    .catch(function(e){
+       
+        res.status(500).json({message:e,success:false})
+        
+    })
+})
+router.get('/employer/reports',function(req,res,next){
+
+    Report.find().sort('-CreatedAt')
+    .then(function(data){
+        
+        res.status(200).json({data,success:true})
+    })
+    .catch(function(e){
+        res.status(400).json({message:e,success:false})
+    })
+    
+
+})
 module.exports = router;
