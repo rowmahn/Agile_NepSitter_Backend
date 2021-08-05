6e3f3a4af02215a3f3747d653813ae3795ed75b6
rowmahn <dulalroman1@gmail.com>
2021-08-05 17:32:53 +0545

    report route added

--- routes/ReportRoute.js
+++ routes/ReportRoute.js
@@ -3,25 +3,37 @@ const router = express.Router();
 const Report = require('../models/Report')
 const authentication=require('../middlewares/authentication')
 
-router.post('/worker/report/:wid', function(req,res){
-    const wid=req.params.wid;
+router.post('/worker/report/:hid', function(req,res){
+    const hireID=req.params.hid;
     const title= req.body.title;
     const issue= req.body.issue;
-    const data=new Report({
-        wid:wid,
-        title:title, 
-        issue:issue
-    })
-    data.save()
-    .then(function(data){
-        res.status(201).json({success:true,data})
+    Hire.findById({_id:hireID}).populate('EmployerID').populate('WorkerID')
+    .then(function(result){
+        const byemployeremail=result.EmployerID.Email
+        const toworkeremail=result.WorkerID.Email
+        const data=new Report({
+            hireID:hireID,
+            title:title, 
+            issue:issue,
+            byemployeremail:byemployeremail,
+            toworkeremail:toworkeremail
+        })
+        data.save()
+        .then(function(data){
+            res.status(201).json({success:true,data})
+    
+        })
+        .catch(function(err){
+           
+            res.status(500).json({message:err,success:false})
+            
+        })
 
     })
     .catch(function(e){
-       
-        res.status(500).json({message:e,success:false})
-        
+        res.status(404).json({message:"hireid not found",success:false})
     })
+    
 })
 router.get('/worker/reports',function(req,res,next){
 
@@ -36,24 +48,34 @@ router.get('/worker/reports',function(req,res,next){
     
 
 })
-router.post('/employer/report/:empid', function(req,res){
-    const empid=req.params.empid;
-    const title= req.body.title;
-    const issue= req.body.issue;
-    const data=new Report({
-        empid:empid,
-        title:title, 
-        issue:issue
-    })
-    data.save()
-    .then(function(data){
-        res.status(201).json({success:true,data})
+
+router.post('/employer/report/:hid', function(req,res){
+    const hireID=req.params.hid;
+    Hire.findById({_id:hireID}).populate('WorkerID').populate('EmployerID')
+    .then(function(result){
+        const toemployeremail=result.EmployerID.Email
+        const  byworkeremail=result.WorkerID.Email
+        const data=new Report({
+            hireID:hireID,
+            title:title, 
+            issue:issue,
+            toemployeremail:toemployeremail,
+            byworkeremail:byworkeremail
+        })
+        data.save()
+        .then(function(data){
+            res.status(201).json({success:true,data})
+    
+        })
+        .catch(function(err){
+           
+            res.status(500).json({message:err,success:false})
+            
+        })
 
     })
     .catch(function(e){
-       
-        res.status(500).json({message:e,success:false})
-        
+        res.status(404).json({message:"hireid not found",success:false})
     })
 })
 router.get('/employer/reports',function(req,res,next){
