f585eeac10509fa9b6a73c5ed83112573b26400a
rowmahn <dulalroman1@gmail.com>
2021-08-06 11:59:08 +0545

    commited

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.port || 4000;
 
 
 app.use(cors(
-    // {credentials: true, origin: 'http://localhost:3000'}
+    // {credentials: true, origin: 'http://localhost:90'}
     ))
     
 
