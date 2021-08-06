b29a64d0a6baac76751c1dbc647377b1fd77026b
rowmahn <dulalroman1@gmail.com>
2021-08-06 10:25:20 +0545

    commited

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors=require('cors');
 const path=require('path')
 const publicDir = path.join(__dirname,'public')
 const app=express();
-const ApplyforjobRoute = require('./Routes/ApplyforjobRoute'); // for CRUD on user
 app.use(express.static(publicDir));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}))
@@ -18,7 +17,7 @@ const port = process.env.port || 90;
 app.use(cors(
     // {credentials: true, origin: 'http://localhost:3000'}
     ))
-    app.use(ApplyforjobRoute);
+    
 
 
 const EmployerRoute=require('./routes/employeerRoute')
@@ -31,6 +30,8 @@ const NotificationRoute=require('./routes/NotificationRoute')
 app.use(NotificationRoute)
 const ReportRoute=require('./routes/ReportRoute')
 app.use(ReportRoute)
+const ApplyforjobRoute = require('./routes/ApplyforjobRoute'); // for CRUD on user
+app.use(ApplyforjobRoute);
 
 
 app.listen(port, ()=>console.log("App listening on port "+ port )
