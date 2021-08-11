b2035f9808cf320ab8a9283c10cab48edcb4032c
yaklesh0007 <yaklesh0007@gmail.com>
2021-08-11 16:33:58 +0545

    change port

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ const ApplyforjobRoute = require('./routes/ApplyforjobRoute'); // for CRUD on us
 app.use(ApplyforjobRoute);
 
 
-app.listen(process.env.PORT || 3001, function(){
+app.listen(process.env.PORT || 90, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
