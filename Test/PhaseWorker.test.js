20e3003c8e9d783932f305fecf9589b587f1d1f8
thilen197 <thilen197@gmail.com>
2021-08-11 17:18:37 +0545

    ädded show all worker test

---
+++ Test/PhaseWorker.test.js
@@ -0,0 +1,19 @@
+import { test } from 'jest-circus'
+import request from 'superagent'
+import supertest from'supertest'
+import app from '../app.js'
+
+
+desbribe ("workers", function(){
+   it('GET /showworkers/all --array worker',()=>{
+   return request(app)
+    .get('/showworkers/all')
+    .set('accept', 'application/json')
+    .expect('content-type' , /json/)
+    .expect(200)
+    // .then((response)=>{
+
+    // })
+   })
+}
+)
