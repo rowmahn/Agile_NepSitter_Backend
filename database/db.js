05d26c474f8cfa9c18e064a1111c86386fe68ee0
rowmahn <dulalroman1@gmail.com>
2021-08-19 13:21:34 +0545

    mongoatlas updated

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://nepsitter:<nepsitter>@cluster0.vxjko.mongodb.net/NepSitterdb?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://nepsitter:nepsitter@cluster0.vxjko.mongodb.net/NepSitterdb?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology : true
