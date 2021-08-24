0512580a23a0653694f2265f672e7cdd819bc19d
thilen197 <thilen197@gmail.com>
2021-08-24 18:41:36 +0545

    TDD Testing for Inserting Employer

---
+++ Test/Employer.test.js
@@ -0,0 +1,73 @@
+const Employeer = require('../models/Employeer');
+const mongoose = require('mongoose');
+
+
+// const db=require('../database/test_db')
+const url = 'mongodb://localhost:27017/thilen';
+beforeAll(async () => {
+await mongoose.connect(url, {
+useNewUrlParser: true,
+useCreateIndex: true
+});
+});
+afterAll(async () => {
+    await mongoose.connection.close();
+    });
+
+ 
+
+    describe('Employer Schema test anything', () => {
+        // the code below is for insert testing
+        it('Add user testing anything', () => {
+        const employeer = {
+
+           'Fullname':'thilen lama',
+            'Contact':'9810285093',
+            'Email':'Thilen198@gmail.com',
+            'Password':'thilen197',
+            'Gender':'male',
+            'Age':'23',
+            'Location':'Aarubari',
+           ' Citizenship':'87018735089274'
+            
+      
+     
+        
+        };
+        return Employeer.create(employeer)
+        .then((pro_ret) => {
+        expect(pro_ret.Email).toEqual('Thilen198@gmail.com');
+        })
+
+ 
+
+    })
+    it('to test the update profile info', async () => {
+    return Employeer.findOneAndUpdate({_id :Object('6124eaa97313100f1c324b38')},
+    {$set : {Fullname:'brad PIIT'}})
+    .then((pp)=>{
+    expect(pp.Fullname).toEqual('brad PIIT')
+    })
+    }); 
+    // get user info
+    it('to test the get profile info', async () => {
+        return Employeer.findById("6124eaa97313100f1c324b38")
+        .then((pp)=>{
+        expect(pp.Fullname).toEqual('brad PIIT')
+        })
+        });
+        
+// // the code below is for delete testing
+// it('to test the delete product is working or not', async () => {
+//     const status = await Employeer.deleteOne({id:"6124eaa97313100f1c324b38"})
+//     expect(status.ok).toBe(1);
+//     })
+
+
+})
+
+ 
+
+
+    
+ 
